d64cf1485d5f01ff49128c6aaf3c072aac3c8aa2
baghelna <54537078+baghelna@users.noreply.github.com>
2020-09-20 23:04:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 st.write("Hello World")
+
+z_data = pd.read_csv(‘https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv 26’)
+z = z_data.values
+
+st.write(1234)
+st.write(pd.DataFrame({'first column': [1, 2, 3, 4],'second column': [10, 20, 30, 40],}))
