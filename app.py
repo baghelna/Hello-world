f9a9e19dd4b27dbc3455eed48cff514c20b75bf2
baghelna <54537078+baghelna@users.noreply.github.com>
2020-09-20 21:39:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,94 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+from plotly.offline import iplot
+import plotly.graph_objs as go
+import plotly.express as px
+from pandas.io.json import json_normalize
+import datetime
 
-def race():
-    st.title('Global Total Cases')
-    st.markdown('### Click Play ▶️ at the bottom of the graph')
-    #st.selectionbox()
-    plots = st.radio('Choose a type of plot',('Bar Plot','Line Plot'))
-    if plots == 'Bar Plot':
-        st.markdown("""<html>
-        <div class="flourish-embed" data-src="story/305795" data-url="https://flo.uri.sh/story/305795/embed">
-        <script src="https://public.flourish.studio/resources/embed.js"></script>
-        <iframe src=\"https://flo.uri.sh/story/305795/embed" frameborder=\"0\" scrolling=\"no\" height=\"810\" width=\"2000\" style=\"width:125%;\"></iframe>
-        </div></html>
-        """, unsafe_allow_html=True)
+# Date Input
+def CovidCases():
+    st.write(datetime.datetime.now())
+    fig = go.Figure()
+    st.write("""# Covid-19 Analysis App""")
+    st.write('Covid-19 came with a seriously shocking series of ripple events. It took the world unaware if we are being honest. Hundreds of thousands of people across nations of the world have been affected greatly by it. ')
+    st.write('So, this app was built to see how well different countries are fairing and the trend in the last 6 months!')
+
+
+    ("""[COVID-19 API](https://documenter.getpostman.com/view/10808728/SzS8rjbc?version=latest#81447902-b68a-4e79-9df9-1b371905e9fa) is used to get the data in this app.""")
+
+
+    url = 'https://api.covid19api.com/countries'
+    r = requests.get(url)
+    df0 = json_normalize(r.json())
+
+    top_row = pd.DataFrame(
+        {'Country': ['Select a Country'], 'Slug': ['Empty'], 'ISO2': ['E']})
+    # Concat with old DataFrame and reset the Index.
+    df0 = pd.concat([top_row, df0]).reset_index(drop=True)
+
+    st.sidebar.header('Filter your search')
+    graph_type = st.sidebar.selectbox(
+        'Cases type', ('confirmed', 'deaths', 'recovered'))
+    st.sidebar.subheader('Search by country ')
+    country = st.sidebar.selectbox('Country', df0.Country)
+    country1 = st.sidebar.selectbox('Compare with another Country', df0.Country)
+    if st.sidebar.button('Refresh Data'):
+        raise RerunException(st.ScriptRequestQueue.RerunData(None))
+
+    if country != 'Select a Country':
+        slug = df0.Slug[df0['Country'] == country].to_string(index=False)[1:]
+        url = 'https://api.covid19api.com/total/dayone/country/'+slug+'/status/'+graph_type
+        r = requests.get(url)
+        st.write("""## Total """+graph_type+""" cases in """ +
+                 country+""" are: """+str(r.json()[-1].get("Cases")))
+        df = json_normalize(r.json())
+        layout = go.Layout(
+            title=country+'\'s '+graph_type+' cases Data',
+            xaxis=dict(title='Date'),
+            yaxis=dict(title='Number of cases'),)
+        fig.update_layout(dict1=layout, overwrite=True)
+        fig.add_trace(go.Scatter(x=df.Date, y=df.Cases,
+                             mode='lines', name=country))
+
+        if country1 != 'Select a Country':
+            slug1 = df0.Slug[df0['Country'] == country1].to_string(index=False)[1:]
+            url = 'https://api.covid19api.com/total/dayone/country/'+slug1+'/status/'+graph_type
+            r = requests.get(url)
+            st.write("""## Total """+graph_type+""" cases in """ +
+                 country1+""" are: """+str(r.json()[-1].get("Cases")))
+            df = json_normalize(r.json())
+            layout = go.Layout(
+                title=country+' vs '+country1+' '+graph_type+' cases Data',
+                xaxis=dict(title='Date'),
+                yaxis=dict(title='Number of cases'),)
+            fig.update_layout(dict1=layout, overwrite=True)
+            fig.add_trace(go.Scatter(x=df.Date, y=df.Cases,
+                                 mode='lines', name=country1))
+
+        st.plotly_chart(fig, use_container_width=True)
+
     else:
-        st.markdown("""<html>
-        <div class="flourish-embed" data-src="story/307671" data-url="https://flo.uri.sh/story/307671/embed">
-        <script src="https://public.flourish.studio/resources/embed.js"></script></div>
-        <iframe src=\"https://flo.uri.sh/story/307671/embed" frameborder=\"0\" scrolling=\"no\" height=\"810\" width=\"2000\" style=\"width:125%;\"></iframe>
-        </div></html>
-        """, unsafe_allow_html=True)
-    st.write('©Nikeshsingh Baghel ©Mehnaz Sheikh')
-    
+        url = 'https://api.covid19api.com/world/total'
+        r = requests.get(url)
+        total = r.json()["TotalConfirmed"]
+        deaths = r.json()["TotalDeaths"]
+        recovered = r.json()["TotalRecovered"]
+        st.write("""# Worldwide Data:""")
+        st.write("Total cases: "+str(total)+", Total deaths: " +
+             str(deaths)+", Total recovered: "+str(recovered))
+        x = ["TotalCases", "TotalDeaths", "TotalRecovered"]
+        y = [total, deaths, recovered]
+
+        layout = go.Layout(
+            title='World Data',
+            xaxis=dict(title='Category'),
+            yaxis=dict(title='Number of cases'),)
+
+        fig.update_layout(dict1=layout, overwrite=True)
+        fig.add_trace(go.Bar(name='World Data', x=x, y=y))
+        st.plotly_chart(fig, use_container_width=True)
 if __name__ == "__main__":
-    race()
+    CovidCases()
